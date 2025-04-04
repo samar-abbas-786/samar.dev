16687fb23ec389c16a611c3f4e9892631f40f335
Samar Abbas <samarabbas172003@gmail.com>
2025-04-04 10:17:01 +0530

    add theme change

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,9 +7,7 @@ const Footer = () => {
   return (
     <footer
       className={`h-10 w-full text-sm flex justify-center items-center transition-colors duration-500 ${
-        dark
-          ? "bg-gradient-to-tr from-[#000000] via-[#101010] to-[#1a1a1a] text-white"
-          : "bg-gray-100 text-black"
+        dark ? "bg-black" : "bg-gray-100 text-black"
       }`}
     >
       <p>Made with ❤️ by Samar {new Date().getFullYear()}</p>
