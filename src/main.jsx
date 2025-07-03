9e06ab050b6693d85da0359cbf331c973291e0be
Samar Abbas <samarabbas172003@gmail.com>
2025-07-03 22:05:31 +0530

    add analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { GlobalState } from "./context/contextApi.jsx";
+import { Analytics } from "@vercel/analytics/next";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <GlobalState>
       <App />
+      <Analytics />
     </GlobalState>
   </React.StrictMode>
 );
