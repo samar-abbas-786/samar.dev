4804b34e80549babffcca3623bfc1504cbe36586
Samar Abbas <samarabbas172003@gmail.com>
2025-07-03 21:24:02 +0530

    chnage default bg

--- src/components/Projects_Section.jsx
+++ src/components/Projects_Section.jsx
@@ -83,6 +83,7 @@ const Projects_Section = () => {
                 <img
                   src={proj.img}
                   alt={proj.title}
+                  loading="lazy"
                   className="w-full h-full object-fit transition-transform duration-500 hover:scale-105"
                 />
                 <div className="absolute inset-0 bg-gradient-to-t from-black/50 to-transparent opacity-0 hover:opacity-100 transition-opacity duration-300 flex items-end p-4">
