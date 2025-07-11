d418153aacc86bccd6f313de4a8c6214fb2a14d1
Samar Abbas <samarabbas172003@gmail.com>
2025-07-12 04:20:51 +0530

    reverst commit

--- src/context/ScrollContext.jsx
+++ src/context/ScrollContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, useRef } from "react";
+import { createContext, useContext, useState, useRef } from "react";
 
 const ScrollContext = createContext();
 
