b435d34c0810175c6429817f1c8d8e709550841c
Samar Abbas <samarabbas172003@gmail.com>
2025-07-11 22:26:48 +0530

    addded analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import "./App.css";
 import Right from "./components/right";
 import { ScrollProvider } from "./context/ScrollContext"; // Import ScrollProvider
 import { useState } from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
-    <ScrollProvider>
-      <div className="top"></div>
-      <div className="main flex flex-col progress-bar">
-        <Right />
-      </div>
-    </ScrollProvider>
+    <>
+      <ScrollProvider>
+        <div className="top"></div>
+        <div className="main flex flex-col progress-bar">
+          <Right />
+        </div>
+      </ScrollProvider>
+      <Analytics />
+    </>
   );
 }
 
